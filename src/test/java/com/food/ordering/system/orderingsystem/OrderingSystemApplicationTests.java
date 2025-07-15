b6b7cbbdabfc2889fa9115b360626efb4b7c5489
kahyun0255 <kkh9946@naver.com>
2025-07-15 15:24:53 +0900

    chore: 초기 환경 설정

---
+++ src/test/java/com/food/ordering/system/orderingsystem/OrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.orderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
