21a5c8613e2eab33a1106b443269694470afa13f
kahyun0255 <kkh9946@naver.com>
2025-07-22 14:30:39 +0900

    feat: Tracking Id로 주문 상태 확인하는 기능 구현

--- src/main/java/com/orderingsystem/order/domain/repository/OrderRepository.java
+++ src/main/java/com/orderingsystem/order/domain/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package com.orderingsystem.order.domain.repository;
 
 import com.orderingsystem.order.domain.model.Order;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    Optional<Order> findByTrackingId(UUID trackingId);
 }
--- src/main/java/com/orderingsystem/order/presentation/OrderController.java
+++ src/main/java/com/orderingsystem/order/presentation/OrderController.java
@@ -2,10 +2,14 @@ package com.orderingsystem.order.presentation;
 
 import com.orderingsystem.order.application.OrderService;
 import com.orderingsystem.order.application.dto.response.CreateOrderResponse;
+import com.orderingsystem.order.application.dto.response.OrderStatusResponse;
 import com.orderingsystem.order.presentation.request.CreateOrderRequest;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +32,11 @@ public class OrderController {
 
         log.info("주문 생성 완료 tracking id : {}", createOrderResponse.getOrderTrackingId());
         return ResponseEntity.ok(createOrderResponse);
+    }
+
+    @GetMapping("/{trackingId}")
+    public ResponseEntity<OrderStatusResponse> getOrderByTrackingId(@PathVariable UUID trackingId) {
+        log.info("주문 추적 시작 : {}", trackingId);
+        return ResponseEntity.ok(orderService.trackOrder(trackingId));
     }
 }
