e802058ccb9de7f78a81a316f4a5abf1257b8bcf
kahyun0255 <kkh9946@naver.com>
2025-07-23 15:21:19 +0900

    fix: application class에 EnableJpaAuditing 추가

--- order/src/main/java/com/OrderApplication.java
+++ order/src/main/java/com/OrderApplication.java
@@ -3,9 +3,11 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OrderApplication {
 
     public static void main(String[] args) {
