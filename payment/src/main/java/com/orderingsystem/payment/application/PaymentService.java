74eb18bff796ae691eb95d845526ae6ad42e24fc
kahyun0255 <kkh9946@naver.com>
2025-11-24 15:27:12 +0900

    fix: 결제 롤백시 credit History OrderId도 같이 저장하도록 수정

--- payment/src/main/java/com/orderingsystem/payment/application/PaymentService.java
+++ payment/src/main/java/com/orderingsystem/payment/application/PaymentService.java
@@ -97,14 +97,14 @@ public class PaymentService {
         List<CreditHistory> creditHistories = getCreditHistories(payment.getCustomerId());
         List<String> failureMessages = new ArrayList<>();
 
-        PaymentEvent paymentEvent = paymentValidateAndCancelService.validateAndCancel(
-                payment, creditEntry, creditHistories, failureMessages);
+        PaymentEvent paymentEvent = paymentValidateAndCancelService.validateAndCancel(payment, creditEntry,
+                creditHistories, failureMessages);
 
         if (failureMessages.isEmpty()) {
             creditHistoryRepository.save(creditHistories.get(creditHistories.size() - 1));
         }
 
-        persistCancelDataBase(payment, creditEntry, creditHistories, failureMessages, payment.getPrice());
+        persistCancelDataBase(payment, creditHistories, failureMessages);
 
         orderOutboxHelper.saveOrderOutboxMessage(
                 paymentDataMapper.paymentEventToOrderEventPayload(paymentEvent, paymentRequest.getSagaId(),
@@ -177,8 +177,8 @@ public class PaymentService {
         }
     }
 
-    private void persistCancelDataBase(Payment payment, CreditEntry creditEntry, List<CreditHistory> creditHistories,
-                                       List<String> failureMassages, Money price) {
+    private void persistCancelDataBase(Payment payment, List<CreditHistory> creditHistories,
+                                       List<String> failureMassages) {
         paymentRepository.save(payment);
         if (failureMassages.isEmpty()) {
             creditHistoryRepository.save(creditHistories.get(creditHistories.size() - 1));
